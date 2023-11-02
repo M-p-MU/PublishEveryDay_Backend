638396063d8ddaa7b3c4d81d26b4f45cb875189e
Rwandantechy <niyinnocent2027@gmail.com>
2023-11-02 18:32:38 +0530

    removed trust proxy

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use(helmet());
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('trust proxy', true);
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 app.set("views", path.join(__dirname, "Views"));
